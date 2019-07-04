cbad9342a20da8a0f83cce1317e1f59579d094d6
anadunning <anakpedrosa@hotmail.com>
2019-07-04 20:55:54 -0300

    Merry Christmaaas!

---
+++ Beginner2483.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int i, l;
+
+    scanf("%d", &l);
+
+    printf("Feliz nat");
+
+    for (i = 0; i < l; i++){
+        printf("a");
+    }
+
+    printf("l!\n");
+
+    return 0;
+}
