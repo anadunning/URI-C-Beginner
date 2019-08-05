9053c786fbdc3d01332ae051f777ddb8e13aa678
anadunning <anakpedrosa@hotmail.com>
2019-08-05 12:02:12 -0300

    The Output

---
+++ Beginner2861.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    int C, i;
+    char question[100];
+
+    scanf("%d\n", &C);
+
+    for(i = 0; i < C; i++){
+
+        gets(question);
+        printf("gzuz\n");
+    }
+
+    return 0;
+}
