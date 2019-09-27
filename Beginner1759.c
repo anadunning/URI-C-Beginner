34c350931cd6670f5dbb702e788bec4d2fcbccac
anadunning <anakpedrosa@hotmail.com>
2019-09-27 20:26:48 -0300

    Ho Ho Ho

---
+++ Beginner1759.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int N, i;
+
+    scanf("%d", &N);
+
+    for (i = 0; i < N - 1; i++){
+        printf("Ho ");
+    }
+    printf("Ho!\n");
+
+    return 0;
+}
