4b29feb888d886a046bc51d50b4c7fc1fe0ada10
anadunning <anakpedrosa@hotmail.com>
2019-10-02 10:37:17 -0300

    Output 3

---
+++ Beginner2749.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+     printf("---------------------------------------\n");
+     printf("|x = 35                               |\n");
+     printf("|                                     |\n");
+     printf("|               x = 35                |\n");
+     printf("|                                     |\n");
+     printf("|                               x = 35|\n");
+     printf("---------------------------------------");
+     printf("\n");
+
+    return 0;
+}
