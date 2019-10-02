20befa39815036ab872b530710ab3eccd1368492
anadunning <anakpedrosa@hotmail.com>
2019-10-02 10:36:14 -0300

    Output 2

---
+++ Beginner2748.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+     printf("---------------------------------------\n");
+     printf("|        Roberto                      |\n");
+     printf("|                                     |\n");
+     printf("|        5786                         |\n");
+     printf("|                                     |\n");
+     printf("|        UNIFEI                       |\n");
+     printf("---------------------------------------");
+     printf("\n");
+
+    return 0;
+}
