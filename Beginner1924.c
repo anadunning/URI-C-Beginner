1ccec3eb5e30a85b3790d068dd822e6aaeff7182
anadunning <anakpedrosa@hotmail.com>
2019-10-09 21:47:52 -0300

    Vitoria and Her Indecision

---
+++ Beginner1924.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    int n, i;
+    char course[50];
+
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++){
+
+        gets(course);
+        getchar();
+    }
+
+    printf("Ciencia da Computacao\n");
+
+    return 0;
+}
