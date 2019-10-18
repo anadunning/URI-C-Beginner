60070e8bf364c39b89949a4bf40e427629fcddcd
anadunning <anakpedrosa@hotmail.com>
2019-10-18 19:13:18 -0300

    Output 7

---
+++ Beginner2753.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    int a, b, c, d, e , f;
+    int g, h, i, j, k, l, m, n, o, p;
+    int q, r, s, t, u, v, w, x, y, z;
+
+    int I;
+    char ch = 'a';
+
+    a = 97;
+
+    printf("%d e a\n", a);
+
+    for (I = 1; I <= 25; I++) {
+
+        printf("%d e %c\n", a+I, ch+I);
+    }
+    return 0;
+}
