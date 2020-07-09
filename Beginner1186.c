f9bb3561dc396da26d0efec6fda2614bb0bb41f0
anadunning <anakpedrosa@hotmail.com>
2020-07-08 23:05:10 -0300

    Below the Secondary Diagonal

---
+++ Beginner1186.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+
+    int i, j;
+    double M[12][12], sum, avg;
+    char Op[2];
+
+    sum = avg = 0;
+
+    scanf("%c", &Op);
+
+    for (i = 0; i < 12; i++){
+        for (j = 0; j < 12; j++){
+            scanf("%lf", &M[i][j]);
+        }
+    }
+
+    if (*Op == 'S'){
+        for (i = 1; i < 12; i++){
+            for (j = 12-i; j < 12; j++){
+               // printf("M[%d][%d] = %.1lf\n", i, j, M);
+                sum += M[i][j];
+            }
+        }
+        printf("%.1lf\n", sum);
+    }
+    else if (*Op == 'M'){
+        for (i = 1; i < 12; i++){
+            for (j = 12-i; j < 12; j++){
+                sum += M[i][j];
+                avg = sum / 66;
+            }
+        }
+        printf("%.1lf\n", avg);
+    }
+
+    return 0;
+}
